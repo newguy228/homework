42ba6419b4012e6cdf62ea9efebfc7e639ef97a5
newguy228 <matvei6447@gmail.com>
2023-03-14 22:14:48 +0400

    Первое задание

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String arg[]) {
+        double p = 70;
+        double s = 25;
+        double d = 53;
+        double c = 19;
+        double t = 41;
+        p = p * 0.77;
+        s = s * 0.37;
+        d = d * 0.44;
+        t = t * 0.32;
+        double sum = p + s + d + c + t;
+        if (sum <= 312) {
+            System.out.println("Нужно " + sum + " серебряных монет. Хватит на покупки");
+        } else {
+            System.out.println("Нужно " + sum + " серебряных монет. Не хватит на покупки");
+        }
+    }
+}
